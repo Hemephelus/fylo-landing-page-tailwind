7662d87c8fa9fa908fa5fbddcb57f3f7fde7f426
Nwachukwu Ujubuonu <nwachukwuujubuonu@gmail.com>
2022-06-17 16:30:20 +0100

    Setup TailWind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+  "./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          SignBg:'#1c2431',
+          FootBg:'#0b1523',
+          MainBg:'#181f2a',
+          TestBg:'#202a3c',
+        },
+        accent:{
+          errorClr: '#ff4242',
+          blueGrd: '#339ecc',
+          CyaneGrd: '#65e2d9',
+        }
+      },
+      fontFamily:{
+        body: ['"Open Sans"'],
+        header: ['Raleway'],
+      },
+    },
+  },
+  plugins: [],
+}
